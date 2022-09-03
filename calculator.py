b3b022a2fa33e9a8e1fe4f153f53d65592948d93
hanzii <pwe1ewq@gmail.com>
2022-09-03 17:23:55 +0900

    calculator

---
+++ calculator.py
@@ -0,0 +1,21 @@
+def plus(a=0, b=0):
+    print(a+b)
+
+def minus(a=0, b=0):
+    print(a-b)
+
+def multiplication(a=0, b=0):
+    print(a*b)
+
+def division(a=0, b=1):
+    print(a/b)
+
+def power(a=0, b=0):
+    print(a**b)
+
+plus()
+minus(4,5)
+multiplication(1,3)
+division()
+power()
+
