838f6c4ce6e535074d65dd6c2fff8d72ca3d31a2
hanzii <pwe1ewq@gmail.com>
2022-09-06 23:07:16 +0900

    상태 코드에 대한 딕셔러니 생성

--- main.py
+++ main.py
@@ -8,9 +8,18 @@ websites =(
     "https://facebook.com"
 )
 
+#웹사이트의 http 상태를 받을 딕셔너리
+results ={}
+
 for website in websites :
     # not를 붙여줌으로써  if문이 false일 때로 설정
+    #웹사이트가 hppts://로 시작하지 않으면 문자를 붙여
     if not website.startswith("https://"):
         website = f"https://{website}"
     response = get(website)
-    print(response.status_code)
+    if response.status_code == 200:
+        results[website] = "OK"
+    else :
+        results[website] = "FAILED"
+
+print(results)
