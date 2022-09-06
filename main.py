af281474c9e7206d34cb64ce75e6f0c04b5ddeaa
hanzii <pwe1ewq@gmail.com>
2022-09-06 23:18:21 +0900

    웹사이트 상태 코드 연습문제

--- main.py
+++ main.py
@@ -2,10 +2,11 @@
 from requests import get
 websites =(
     "google.com",
-    "naver.com",
-    "airbnb.com",
-    "https://twitter.com",
-    "https://facebook.com"
+    "https://httpstat.us/502",
+    "https://httpstat.us/404",
+    "https://httpstat.us/300",
+    "https://httpstat.us/200",
+    "https://httpstat.us/101"
 )
 
 #웹사이트의 http 상태를 받을 딕셔너리
@@ -17,9 +18,16 @@ for website in websites :
     if not website.startswith("https://"):
         website = f"https://{website}"
     response = get(website)
-    if response.status_code == 200:
-        results[website] = "OK"
-    else :
-        results[website] = "FAILED"
+    code = response.status_code
+    if code >= 100 and code < 200:
+        results[website] = "1xx/impormation responses"
+    elif code >= 200 and code < 300:
+        results[website] = "2xx/Successful"
+    elif code >= 300 and code < 400:
+        results[website] = "3xx/Redirection"
+    elif code >= 400 and code < 500:
+        results[website] = "4xx/Client error"
+    elif code >= 500 and code < 600:
+        results[website] = "5xx/Server error"
 
 print(results)
