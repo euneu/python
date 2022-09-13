347903819876a40b9916db2d3f9b99e5d543b2f1
hanzii <pwe1ewq@gmail.com>
2022-09-14 01:55:06 +0900

    셀리니움 설치

--- main.py
+++ main.py
@@ -2,6 +2,11 @@
 from requests import get
 from bs4 import BeautifulSoup
 from extractors.wwr import extract_wwr_jobs
+from selenium import webdriver
 
-jobs = extract_wwr_jobs("python")
-print(jobs)
+browser = webdriver.Chrome('./chromedriver.exe')
+
+base_url = "https://www.indeed.com/jobs?q="
+search_term = "python"
+
+browser.get("https://naver.com")
