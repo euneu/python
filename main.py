a5bf01b042e7d94677ead14112a1b8cb69a48de5
hanzii <pwe1ewq@gmail.com>
2022-09-14 16:31:30 +0900

    none

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from requests import get
 from bs4 import BeautifulSoup
 from extractors.wwr import extract_wwr_jobs
@@ -6,7 +5,18 @@ from selenium import webdriver
 
 browser = webdriver.Chrome('./chromedriver.exe')
 
-base_url = "https://www.indeed.com/jobs?q="
+base_url = "https://kr.indeed.com/jobs?q="
 search_term = "python"
 
-browser.get("https://naver.com")
+browser.get(f"{base_url}{search_term}")
+
+soup = BeautifulSoup(browser.page_source, "html.parser")
+job_list = (soup.find("ul", class_="jobsearch-ResultsList"))
+jobs = job_list.find_all('li', recursive=False)
+print(len(jobs))
+for job in jobs:
+    zone = job.find("div", class_="mosaic-zone")
+    if zone == None :
+        print("job li")
+    else :
+        print("mosaic li")
